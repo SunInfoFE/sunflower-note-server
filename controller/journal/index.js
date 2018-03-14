a8dfacd38e821f5351ca4c892c7ff1a6c8283fe1
webCaolixin <292354215@qq.com>
2018-03-14 16:55:11 +0800

    添加journal-本周日志模块API

--- controller/journal/index.js
+++ controller/journal/index.js
@@ -10,7 +10,7 @@ const getMonday = require('../../common/utils/getMonday');
  * @param next
  * @returns {Promise.<void>}
  */
-let get = async (ctx, next) => {
+let getAll = async (ctx, next) => {
   const getSql = 'SELECT * FROM journal_info WHERE email = ? AND week = ?'
   try {
     let getData = await dbQuery(getSql, [ctx.session.userId, getMonday()]);
@@ -32,6 +32,104 @@ let get = async (ctx, next) => {
 
 
 /**
+ * 新增本周工作日志
+ * @param ctx
+ * @param next
+ * @returns {Promise.<void>}
+ */
+let add = async (ctx, next) => {
+  const addSql = 'INSERT INTO journal_info (task, email, week) VALUES (?, ?, ?, ?)';
+  try {
+    let addData = await dbQuery(addSql, [ctx.request.body.task, ctx.session.userId, getMonday()]);
+    if (addData.affectedRows === 1) {
+      ctx.body = {
+        status: true,
+        data: '新增成功！'
+      }
+    } else {
+      ctx.status = 500;
+      ctx.body = {
+        status: true,
+        data: addData
+      }
+    }
+  } catch(err) {
+    console.log(`${ctx.method} - ${ctx.url} ERROR -- ${err}`);
+    ctx.status = 500;
+    ctx.body = {
+      status: true,
+      data: err.message
+    }
+  }
+};
+
+
+/**
+ * 编辑本周工作日志
+ * @param ctx
+ * @param next
+ * @returns {Promise.<void>}
+ */
+let edit = async (ctx, next) => {
+  const editSql = 'UPDATE journal_info SET task = ?, createTime = createTime WHERE id = ?';
+  try {
+    let editData = await dbQuery(editSql, [ctx.request.body.task, ctx.request.body.id]);
+    if (editData.changedRows === 1) {
+      ctx.body = {
+        status: true,
+        data: '编辑成功！'
+      }
+    } else {
+      ctx.status = 500;
+      ctx.body = {
+        status: true,
+        data: editData
+      }
+    }
+  } catch(err) {
+    console.log(`${ctx.method} - ${ctx.url} ERROR -- ${err}`);
+    ctx.status = 500;
+    ctx.body = {
+      status: true,
+      data: err.message
+    }
+  }
+};
+
+/**
+ * 修改工作日志状态 status
+ * @param ctx
+ * @param next
+ * @returns {Promise.<void>}
+ */
+let changeStatus = async (ctx, next) => {
+  const changeSql = 'UPDATE journal_info SET status = ?, createTime = createTime WHERE id = ?';
+  try {
+    let changeData = await dbQuery(changeSql, [ctx.request.body.status, ctx.request.body.id]);
+    if (changeData.changedRows === 1) {
+      ctx.body = {
+        status: true,
+        data: '状态更改成功！'
+      }
+    } else {
+      ctx.status = 500;
+      ctx.body = {
+        status: true,
+        data: changeData
+      }
+    }
+  } catch(err) {
+    console.log(`${ctx.method} - ${ctx.url} ERROR -- ${err}`);
+    ctx.status = 500;
+    ctx.body = {
+      status: true,
+      data: err.message
+    }
+  }
+};
+
+
+/**
  * 将所选'工作记录'形成的预览周报，保存为周报草稿
  * @param ctx
  * @param next
@@ -41,11 +139,11 @@ let saveDraft = async (ctx, next) => {
   try {
     const saveSql = "INSERT INTO report_info (title, summary, plan, week, email, groupId) SELECT ?, ?, ?, ?, ?, groupId FROM user_info WHERE email = ?"
     let {title, summary, plan} = ctx.request.body;
-    let saveData = await dbQuery(saveSql, ['未命名周报', summary, '计划', getMonday(), ctx.session.userId, ctx.session.userId]);
+    let saveData = await dbQuery(saveSql, [title, summary, plan, getMonday(), ctx.session.userId, ctx.session.userId]);
     if (saveData.affectedRows === 1) {
       ctx.body = {
         status: true,
-        data: saveData
+        data: '保存成功，可在"我的周报"中查看！"'
       }
     } else {
       ctx.body = {
@@ -71,11 +169,46 @@ let saveDraft = async (ctx, next) => {
  * @returns {Promise.<void>}
  */
 let submitDraft = async (ctx, next) => {
+  const changeSql = "UPDATE report_info SET status = 'private' WHERE email = ? AND week = ? AND status = 'public'";
+  try {
+    let changeData = await dbQuery(changeSql, [ctx.session.userId, getMonday()]);
+    if (changeData) {
+      const saveSql = "INSERT INTO report_info (title, summary, plan, week, email, groupId) SELECT ?, ?, ?, ?, ?, groupId FROM user_info WHERE email = ?"
+      let {title, summary, plan} = ctx.request.body;
+      let saveData = await dbQuery(saveSql, [title, summary, plan, getMonday(), ctx.session.userId, ctx.session.userId]);
+      if (saveData.affectedRows === 1) {
+        ctx.body = {
+          status: true,
+          data: '提交成功'
+        }
+      } else {
+        ctx.body = {
+          status: false,
+          data: saveData
+        }
+      }
+    } else {
+      ctx.body = {
+        status: false,
+        data: changeData
+      }
+    }
+  } catch(err) {
+    console.log(`${ctx.method} - ${ctx.url} ERROR -- ${err}`);
+    ctx.status = 500;
+    ctx.body = {
+      status: false,
+      data: err.message
+    }
+  }
 };
 
 
 module.exports = {
-  get,
+  getAll,
+  add,
+  edit,
+  changeStatus,
   saveDraft,
   submitDraft
 }
--- routers/journal.js
+++ routers/journal.js
@@ -8,9 +8,39 @@ router.prefix('/journal');
 
 /**
  * 获取所有本周工作日志接口
- * url: /journal/getAllCurrentWeekJournal
+ * url: /journal/currentWeekJournal/getAll
  */
-router.get('/getAllCurrentWeekJournal', journal.get);
+router.get('/currentWeekJournal/getAll', journal.getAll);
+
+/**
+ * 新增本周工作日志接口
+ * url: /journal/currentWeekJournal/add
+ */
+router.post('/currentWeekJournal/add', journal.add);
+
+/**
+ * 编辑本周工作日志接口
+ * url: /journal/currentWeekJournal/edit
+ */
+router.post('/currentWeekJournal/edit', journal.edit);
+
+/**
+ * 修改工作日志状态 status 接口
+ * url: /journal/currentWeekJournal/changeStatus
+ */
+router.post('/currentWeekJournal/changeStatus', journal.changeStatus);
+
+/**
+ * 将所选'工作记录'形成的预览周报，保存为周报草稿接口
+ * url: /journal/currentWeekJournal/saveDraft
+ */
+router.post('/currentWeekJournal/saveDraft', journal.saveDraft);
+
+/**
+ * 将所选'工作记录'形成的预览周报，直接提交至小组周报接口
+ * url: /journal/currentWeekJournal/submitDraft
+ */
+router.post('/currentWeekJournal/submitDraft', journal.submitDraft);
 
 
 module.exports = router;
