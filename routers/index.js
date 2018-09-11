41de1b61ffa51134c44f139c09053359d241b7d6
alfredodiego <alfredodiego@126.com>
2018-09-11 09:31:04 +0800

    打卡

--- routers/index.js
+++ routers/index.js
@@ -8,6 +8,7 @@ const report = require('./report');
 const group = require('./group');
 const system = require('./system');
 const journal = require('./journal');
+const card = require('./card');
 
 /**
  * 统一挂载router级中间件路由
@@ -17,5 +18,6 @@ router.use(report.routes(), report.allowedMethods());
 router.use(group.routes(), group.allowedMethods());
 router.use(system.routes(), system.allowedMethods());
 router.use(journal.routes(), journal.allowedMethods());
+router.use(card.routes(), card.allowedMethods());
 
 module.exports = router;
