3af9864d216f29a1451c59c55d6c81e3ba82375a
UNformed <839085944@qq.com>
2018-11-26 13:33:57 +0800

    增加点餐功能

--- doc/sunflower-note.sql
+++ doc/sunflower-note.sql
@@ -1,148 +1,140 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : MySQL
-Source Server Version : 50720
-Source Host           : localhost:33060
-Source Database       : weeklyreport_db
+ Source Server         : weeklyreportConnection
+ Source Server Type    : MySQL
+ Source Server Version : 50528
+ Source Host           : localhost:3306
+ Source Schema         : weeklyreport_db
 
-Target Server Type    : MYSQL
-Target Server Version : 50720
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50528
+ File Encoding         : 65001
 
-Date: 2018-04-12 16:13:58
+ Date: 26/11/2018 13:31:59
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for `email_info`
--- ----------------------------
-DROP TABLE IF EXISTS `email_info`;
-CREATE TABLE `email_info` (
-  `email` char(30) COLLATE utf8_unicode_ci NOT NULL,
-  `licenseKey` char(30) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `status` enum('unactivated','activated') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'unactivated',
-  `activeCode` char(50) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`email`,`activeCode`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- ----------------------------
--- Records of email_info
--- ----------------------------
-
--- ----------------------------
--- Table structure for `group_info`
--- ----------------------------
-DROP TABLE IF EXISTS `group_info`;
-CREATE TABLE `group_info` (
-  `id` int(20) NOT NULL AUTO_INCREMENT,
-  `name` char(30) COLLATE utf8_unicode_ci NOT NULL,
-  `memberNum` int(4) DEFAULT '0',
-  `remark` char(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `combine` tinyint(1) unsigned zerofill NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- ----------------------------
--- Records of group_info
--- ----------------------------
-
--- ----------------------------
--- Table structure for `journal_info`
--- ----------------------------
-DROP TABLE IF EXISTS `journal_info`;
-CREATE TABLE `journal_info` (
-  `id` int(20) NOT NULL AUTO_INCREMENT,
-  `task` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `email` char(30) COLLATE utf8_unicode_ci NOT NULL,
-  `status` enum('finished','unfinished') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'unfinished',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- ----------------------------
--- Records of journal_info
--- ----------------------------
-
--- ----------------------------
--- Table structure for `report_info`
--- ----------------------------
-DROP TABLE IF EXISTS `report_info`;
-CREATE TABLE `report_info` (
-  `id` int(20) NOT NULL AUTO_INCREMENT,
-  `title` char(100) COLLATE utf8_unicode_ci NOT NULL,
-  `summary` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `plan` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `lastUpdateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `week` date NOT NULL,
-  `status` enum('public','private') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'private',
-  `email` char(30) COLLATE utf8_unicode_ci NOT NULL,
-  `groupId` int(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=122 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- ----------------------------
--- Records of report_info
--- ----------------------------
-
--- ----------------------------
--- Table structure for `sendmail_info`
--- ----------------------------
-DROP TABLE IF EXISTS `sendmail_info`;
-CREATE TABLE `sendmail_info` (
-  `email` char(30) COLLATE utf8_unicode_ci NOT NULL,
-  `name` char(20) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- ----------------------------
--- Records of sendmail_info
--- ----------------------------
-
--- ----------------------------
--- Table structure for `user_info`
--- ----------------------------
-DROP TABLE IF EXISTS `user_info`;
-CREATE TABLE `user_info` (
-  `email` char(30) COLLATE utf8_unicode_ci NOT NULL,
-  `name` char(20) COLLATE utf8_unicode_ci NOT NULL,
-  `sex` enum('female','male') COLLATE utf8_unicode_ci DEFAULT NULL,
-  `remark` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `groupId` int(20) NOT NULL,
-  `password` char(30) COLLATE utf8_unicode_ci NOT NULL,
-  `role` enum('user','admin') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'user',
-  `level` tinyint(1) unsigned zerofill NOT NULL,
-  `collector` tinyint(1) unsigned zerofill NOT NULL,
-  PRIMARY KEY (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- ----------------------------
--- Records of user_info
--- ----------------------------
-
--- ----------------------------
--- Table structure for `_mysql_session_store`
+-- Table structure for _mysql_session_store
 -- ----------------------------
 DROP TABLE IF EXISTS `_mysql_session_store`;
-CREATE TABLE `_mysql_session_store` (
-  `id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `expires` bigint(20) DEFAULT NULL,
-  `data` text COLLATE utf8_unicode_ci,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE `_mysql_session_store`  (
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `expires` bigint(20) NULL DEFAULT NULL,
+  `data` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Compact;
 
+-- ----------------------------
+-- Table structure for email_info
+-- ----------------------------
+DROP TABLE IF EXISTS `email_info`;
+CREATE TABLE `email_info`  (
+  `email` char(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `licenseKey` char(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `status` enum('unactivated','activated') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'unactivated',
+  `activeCode` char(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`email`, `activeCode`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for group_info
+-- ----------------------------
+DROP TABLE IF EXISTS `group_info`;
+CREATE TABLE `group_info`  (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `name` char(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `memberNum` int(4) NULL DEFAULT 0,
+  `remark` char(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `combine` tinyint(1) UNSIGNED ZEROFILL NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 49 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for ip_info
+-- ----------------------------
+DROP TABLE IF EXISTS `ip_info`;
+CREATE TABLE `ip_info`  (
+  `ip` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `groups` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `member` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `job` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `remarks` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `unused` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`ip`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for journal_info
+-- ----------------------------
+DROP TABLE IF EXISTS `journal_info`;
+CREATE TABLE `journal_info`  (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `task` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `email` char(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `status` enum('finished','unfinished') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'unfinished',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 409 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for punch_card
+-- ----------------------------
 DROP TABLE IF EXISTS `punch_card`;
-CREATE TABLE `punch_card` (
+CREATE TABLE `punch_card`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userid` varchar(100) DEFAULT NULL,
-  `card_time` varchar(30) DEFAULT NULL,
-  `card_status` int(1) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `userid` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `card_time` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `card_status` int(1) NULL DEFAULT NULL,
+  `signed_time` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `off_time` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 128 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
--- Records of _mysql_session_store
+-- Table structure for sendmail_info
 -- ----------------------------
+DROP TABLE IF EXISTS `sendmail_info`;
+CREATE TABLE `sendmail_info`  (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `email` char(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `name` char(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for today_dinner
+-- ----------------------------
+DROP TABLE IF EXISTS `today_dinner`;
+CREATE TABLE `today_dinner`  (
+  `dinner` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `mount` int(255) NULL DEFAULT NULL,
+  `selected` int(255) NULL DEFAULT NULL,
+  `date` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
+  `time` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
+  PRIMARY KEY (`dinner`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for user_info
+-- ----------------------------
+DROP TABLE IF EXISTS `user_info`;
+CREATE TABLE `user_info`  (
+  `email` char(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `name` char(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `sex` enum('female','male') CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `groupId` int(20) NOT NULL,
+  `password` char(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `role` enum('user','admin') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'user',
+  `collector` tinyint(1) UNSIGNED ZEROFILL NOT NULL DEFAULT 0,
+  `level` tinyint(1) UNSIGNED ZEROFILL NOT NULL DEFAULT 0,
+  `dinner` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
+  PRIMARY KEY (`email`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
