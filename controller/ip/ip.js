47db7bbfbada62544e8f8453e214c1e5bf065060
UNformed <839085944@qq.com>
2019-01-04 14:34:09 +0800

    ip管理

--- controller/ip/ip.js
+++ controller/ip/ip.js
@@ -2,7 +2,7 @@ const query = require('../../lib/mysql.js')
 // 获取所有ip信息
 let getAllIpData = async (ctx, next) => {
     try {
-        let sql = `SELECT * FROM ip_info`
+        let sql = `SELECT * FROM ip_info ORDER BY segment,id ASC`
         let result = await query(sql)
         if(result instanceof Array && result.length > 0) {
             ctx.body = {
@@ -101,10 +101,162 @@ let getMyIp = async (ctx, next) => {
         }
     }
 }
+// 添加一个网段机器对应所有ip
+let addNetSegment = async(ctx, next) => {
+    try {
+        let {segment} = ctx.request.body
+        //console.log(segment)
+        let result = null
+        let affectedRows = 0
+        let insertSql = `INSERT INTO ip_info (ip,used,remarks,email,name,segment) VALUES (?,'0','','','',?)`
+        let a = 509
+        for(let i = 1; i < 255; i++) {
+            result = await query(insertSql, [`192.168.${segment}.${i}`, segment])
+            //console.log(`192.168.211.${i}`)
+            if(result.affectedRows === 1) {
+                affectedRows ++
+            }
+            a ++
+        }
+        if(affectedRows === 254) {
+            ctx.body = {
+                status: true,
+                data: []
+            }
+        } else {
+            ctx.body = {
+                status: false,
+                data: []
+            }
+        }
+    } catch(err) {
+        console.log(`${ctx.method} - ${ctx.url} -- ERROR -- ${err}`)
+        ctx.body = {
+            status: false,
+            data: []
+        }
+    }
+}
+let setSegment = async(ctx, next) => {
+    try {
+        let sql = `SELECT ip from ip_info`
+        let result = await query(sql)
+        let sql2 = `UPDATE ip_info SET segment=? where ip=?`
+        let result2 = null
+        let segment = ''
+        for(let i = 0; i < result.length; i ++) {
+            segment = String(result[i].ip).split('.')[2]
+            result2 = await query(sql2, [segment, result[i].ip])
+        }
+        ctx.body = {
+            status: true,
+            data: []
+        }
+    } catch(e) {
+
+    }
+}
+// 删除整个网段的ip
+let deleteSegment = async(ctx, next) => {
+    try {
+        let {segment} = ctx.request.body
+        let sql = `DELETE FROM ip_info WHERE segment=?`
+        let result = await query(sql, [segment])
+        if (result.affectedRows > 0) {
+            ctx.body = {
+                status: true,
+                data: []
+            }
+        } else {
+            ctx.body = {
+                status: false,
+                data: []
+            }
+        }
+    } catch(err) {
+        console.log(`${ctx.method} - ${ctx.url} -- ERROR -- ${err}`)
+        ctx.body = {
+            status: false,
+            data: []
+        }
+    }
+}
+// 计算某个网段中包使用的ip的数量
+let count = async(ctx, next) => {
+    let {segment} = ctx.request.body
+    let sql = `SELECT COUNT(*) FROM ip_info WHERE segment=? AND used=1`
+    let result = await query(sql, [segment])
+    let num = 0
+    for (const x in result[0]) {
+        if (result[0].hasOwnProperty(x)) {
+            num = result[0][x];  
+        }
+    }
+    ctx.body = {
+        status: true,
+        data: num
+    }
+}
+// 获取所有网段的种类
+let getSegment = async(ctx, next) => {
+    try {
+        let sql = `SELECT DISTINCT segment FROM ip_info ORDER BY segment ASC`
+        let result = await query(sql);
+        if(result instanceof Array && result.length > 0) {
+            ctx.body = {
+                status: true,
+                data: result
+            }
+        } else {
+            ctx.body = {
+                status: false,
+                data: []
+            }
+        }
+    } catch(err) {
+        console.log(`${ctx.method} - ${ctx.url} -- ERROR -- ${err}`)
+        ctx.body = {
+            status: false,
+            data: []
+        }
+    }
+}
+// 获取某一网段的所有ip信息
+let getIpBySegment = async(ctx, next) => {
+    try {
+        let {segment} = ctx.request.body
+        let sql = `SELECT * FROM ip_info WHERE segment=?`
+        let result = await query(sql, segment)
+        console.log(result)
+        if(result instanceof Array && result.length > 0) {
+            ctx.body = {
+                status: true,
+                data: result
+            }
+        } else {
+            ctx.body = {
+                status: false,
+                data: []
+            }
+        }
+    } catch(err) {
+        console.log(`${ctx.method} - ${ctx.url} -- ERROR -- ${err}`)
+        ctx.body = {
+            status: false,
+            data: []
+        }
+    }
+}
 
 module.exports = {
     getAllIpData,
     returnIp,
     getMyIp,
-    applyIp
+    applyIp,
+    addNetSegment,
+    setSegment,
+    deleteSegment,
+    count,
+    getSegment,
+    getIpBySegment
 }
--- routers/ip.js
+++ routers/ip.js
@@ -7,5 +7,11 @@ router.get('/getmyip', ip.getMyIp)
 router.get('/getallip', ip.getAllIpData)
 router.post('/returnip', ip.returnIp)
 router.post('/applyip', ip.applyIp)
+router.post('/addsegment', ip.addNetSegment)
+router.get('/setsegment', ip.setSegment)
+router.post('/deletesegment', ip.deleteSegment)
+router.post('/getcount', ip.count)
+router.get('/getsegment', ip.getSegment)
+router.post('/getipbysegment', ip.getIpBySegment)
 
 module.exports = router
